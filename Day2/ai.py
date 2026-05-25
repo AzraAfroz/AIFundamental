1641480703e6fbcb450607c2fbcc3831910c9956
Azra Afroz <azraafroz859@gmail.com>
2026-05-25 16:12:27 +0530

    day 2

---
+++ Day2/ai.py
@@ -0,0 +1,16 @@
+# Neural Network Basic Logic
+
+# Input Layer
+study_hours = 5
+
+# Weight
+weight = 2
+
+# Hidden Layer Calculation
+hidden_output = study_hours * weight
+
+# Output Layer
+if hidden_output > 8:
+    print("Pass")
+else:
+    print("Fail")
