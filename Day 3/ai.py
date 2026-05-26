cb78bff6c4cf5a391cf5ee72bab6037e96f60ec0
Azra Afroz <azraafroz859@gmail.com>
2026-05-26 16:30:29 +0530

    day 3

---
+++ Day 3/ai.py
@@ -0,0 +1,10 @@
+# Simple Next Word Prediction
+
+sentence = "I drink coffee every"
+
+possible_words = ["morning", "car", "banana"]
+
+# AI chooses most likely word
+prediction = "morning"
+
+print(sentence + " " + prediction)
